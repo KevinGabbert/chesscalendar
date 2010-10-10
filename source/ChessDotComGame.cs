9c7dcbd0ba08d5695bd0e7c763115e2ee85deda0
kevingabbert@gmail.com <kevingabbert@gmail.com@0b401191-2a60-48e8-5509-3f5dc672fb8b>
2010-10-10 05:01:42 +0000

    Added the chess.com object.  Some planning.

---
+++ source/ChessDotComGame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ChessCalendar
+{
+    class ChessDotComGame
+    {
+        public string Comments { get; set; }
+        public string Description { get; set; }
+        public string Guid { get; set; }
+        public string Link { get; set; }
+        public string PubDate { get; set; }
+        public string Title { get; set; }
+
+        public string Rating
+        {
+            get
+            {
+                return Description.Split("<br/>".ToArray(), StringSplitOptions.None)[0];
+            }
+        }       
+    }
+}
