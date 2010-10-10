a98f9b3dbd010ddb03a0ec5d2f02f4e8b4971b11
kevingabbert@gmail.com <kevingabbert@gmail.com@0b401191-2a60-48e8-5509-3f5dc672fb8b>
2010-10-10 02:54:21 +0000

    Starting with code stolen from my process-calendar project (also on Google code)
    Did a basic test to make sure that I can access the RSS feed of chess.com.  
    
    Login Form has been created. I haven't gone over the rest of the objects imported over from the process-calendar app, so things are a bit of a mess at present.
    
    App does not work when you run it yet, so don't try.

---
+++ trunk/source/Login_Form.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Forms;
+using Google.GData.Calendar;
+
+namespace ChessCalendar
+{
+    /// <summary>
+    /// This form is designed to pop up whenever login information is not known (and pre-populated)
+    /// normally, the user would never see this form, and if they do, it would be rare.
+    /// </summary>
+    public partial class Login_Form : Form
+    {
+        #region Properties
+
+        public string User { get; set; }
+        public string Password { get; set; }
+        public System.Uri PostURI { get; set; }
+
+        #endregion
+
+        public Login_Form()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.User = this.txtLogin.Text;
+            this.Password = this.txtPassword.Text;
+
+            cmbGoogleCalendar.Items.Clear();
+            CalendarFeed calFeed = GoogleCalendar.RetrieveCalendars(this.User, this.Password);
+            foreach (CalendarEntry centry in calFeed.Entries)
+            {
+                cmbGoogleCalendar.Items.Add(centry);
+            }
+
+            cmbGoogleCalendar.DisplayMember = "Title";
+            cmbGoogleCalendar.ValueMember = "Title";
+            if (cmbGoogleCalendar.Items.Count > 0)
+            {
+                cmbGoogleCalendar.SelectedIndex = 0;
+            }
+        }
+
+        private void LoginTextControls_TextChanged(object sender, EventArgs e)
+        {
+            TextBoxBase senderTB = (TextBoxBase)sender;
+
+            if (senderTB.Text.Contains(Environment.NewLine))
+            {
+                this.btnOK_Click(sender, e);
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //do we need some kind of validation here?
+
+            this.SetPostURI();
+            this.Hide();
+        }
+
+        private void cmbGoogleCalendar_Leave(object sender, EventArgs e)
+        {
+            this.SetPostURI();
+        }
+
+        private void SetPostURI()
+        {
+            this.PostURI = new Uri("http://www.google.com/calendar/feeds/" +
+                      ((CalendarEntry)(cmbGoogleCalendar.SelectedItem)).SelfUri.ToString().Substring(((CalendarEntry)(cmbGoogleCalendar.SelectedItem)).SelfUri.ToString().LastIndexOf("/") + 1) +
+                      "/private/full");
+        }
+    }
+}
---
+++ trunk/source/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChessCalendar
+{
+    class Program
+    {
+        public const string VERSION = "Game Calendar v10.09.10 ";
+        public const string CONFIG_FILE_PATH = @"..\..\GamesToLog.xml";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(VERSION + DateTime.Now.ToShortTimeString());
+
+            //var userInfoForm = new Login_Form();
+            //userInfoForm.ShowDialog();  
+            
+            //*** Code Execution will stop at this point and wait until user has dismissed the Login form. ***//
+
+            Log.LogVersion = VERSION;
+            Log.Log_All_Games(new Uri("http://www.chess.com/rss/echess/KevinGabbert"));
+
+            //Log.Log_Watched_Games(Config.GetWatchList(CONFIG_FILE_PATH), userInfoForm.User, userInfoForm.Password, userInfoForm.PostURI);
+        } 
+    }
+}
