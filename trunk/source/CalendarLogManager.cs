1e30ee3949be1a728a14095292ef2a86aba2b17d
kevingabbert@gmail.com <kevingabbert@gmail.com@0b401191-2a60-48e8-5509-3f5dc672fb8b>
2010-10-16 06:04:39 +0000

    Cleanup refactor.

--- trunk/source/CalendarLogManager.cs
+++ trunk/source/CalendarLogManager.cs
@@ -1,45 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using RssToolkit.Rss;
 
 namespace ChessCalendar
 {
-    public class GameList: List<ChessDotComGame>
-    {
-        public void Remove_Item_With_Guid(string link)
-        {
-            this.Remove_Item_With_Guid(this, link);
-        }
-        public void Remove_Item_With_Guid(IList<ChessDotComGame> listToRemoveFrom, string link)
-        {
-            for (int i = listToRemoveFrom.Count() - 1; i > -1; i--)
-            {
-                var currentGame = listToRemoveFrom[i];
-
-                if (currentGame.Link == link)
-                {
-                    Console.WriteLine("Removing Game: " + currentGame.Title);
-                    listToRemoveFrom.Remove(currentGame);
-                    break;
-                }
-            }
-        }
-
-        public void AddNewGame(RssItem rssItem)
-        {
-            var game = new ChessDotComGame();
-            game.Title = rssItem.Title;
-            game.Link = rssItem.Link;
-            game.PubDate = rssItem.PubDate;
-            game.Comments = rssItem.Comments;
-            game.Guid = rssItem.Guid;
-
-            base.Add(game);
-            //If DebugMode ~ Console.WriteLine("Added " + game.Title + " " + game.PubDate);
-        }
-    }
-
     public class CalendarLogManager : GameList
     {
         #region Properties
@@ -82,7 +46,7 @@ namespace ChessCalendar
             }
         }
 
-        public void ProcessItem(RssItem rssItem)
+        public void ProcessRSSItem(RssItem rssItem)
         {
             this.IgnoreIfWeHaveIt(rssItem);
 
