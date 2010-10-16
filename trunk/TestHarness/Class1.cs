d48199f0da605be38ac0f11058930a0d6b286881
kevingabbert@gmail.com <kevingabbert@gmail.com@0b401191-2a60-48e8-5509-3f5dc672fb8b>
2010-10-16 22:32:18 +0000

    Added Test Harness

---
+++ trunk/TestHarness/Class1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace ChessCalendar.TestHarness
+{
+    [TestFixture]
+    public class Class1
+    {
+
+        [Test]
+        public void Test()
+        {
+            
+        }
+    }
+}
