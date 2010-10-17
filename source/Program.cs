331cd04d2457b968dc4316917b4ac9c9abbebcc3
kevingabbert@gmail.com <kevingabbert@gmail.com@0b401191-2a60-48e8-5509-3f5dc672fb8b>
2010-10-17 22:01:22 +0000

    Fixed a login bug

--- source/Program.cs
+++ source/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 
 namespace ChessCalendar
 {
@@ -11,15 +12,25 @@ namespace ChessCalendar
         {
             Console.WriteLine(VERSION + DateTime.Now.ToShortTimeString());
 
+            ShowDialog:
             var userInfoForm = new Login_Form(VERSION);
             userInfoForm.ShowDialog();  
             
             //*** Code Execution will stop at this point and wait until user has dismissed the Login form. ***//
 
-            Log.LogVersion = VERSION;
-            Log.DebugMode = userInfoForm.DebugMode;
-            Log.Beep_On_New_Move = userInfoForm.Beep_On_New_Move;
-            Log.Log_All_Games(new Uri("http://www.chess.com/rss/echess/" + userInfoForm.ChessDotComName), userInfoForm.User, userInfoForm.Password, userInfoForm.PostURI);
+            if (userInfoForm.ValidatedForm)
+            {
+                Log.LogVersion = VERSION;
+                Log.DebugMode = userInfoForm.DebugMode;
+                Log.Beep_On_New_Move = userInfoForm.Beep_On_New_Move;
+                Log.Log_All_Games(new Uri("http://www.chess.com/rss/echess/" + userInfoForm.ChessDotComName),
+                                  userInfoForm.User, userInfoForm.Password, userInfoForm.PostURI);
+            }
+            else
+            {
+                MessageBox.Show("Some of the Login information wasn't right, try again.", "ummmmmmm...");
+                goto ShowDialog; //woohoo! lookit that goto!
+            }
         } 
     }
 }
