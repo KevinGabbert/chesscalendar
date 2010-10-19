f720da68ec6e7f119b89d665feabed402412aeb7
kevingabbert@gmail.com <kevingabbert@gmail.com@0b401191-2a60-48e8-5509-3f5dc672fb8b>
2010-10-19 06:48:00 +0000

    *** Revision 1 ***

---
+++ tags/Revision 1/source/GameList.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ChessCalendar.Interfaces;
+using Google.GData.Client;
+using RssToolkit.Rss;
+
+namespace ChessCalendar
+{
+    public class GameList : List<IChessItem>
+    {
+        public bool DebugMode { get; set; }
+
+        public void Remove_Item_With_Guid(string link)
+        {
+            this.Remove_Item_With_Guid(this, link);
+        }
+        public void Remove_Item_With_Guid(IList<IChessItem> listToRemoveFrom, string link)
+        {
+            for (int i = listToRemoveFrom.Count() - 1; i > -1; i--)
+            {
+                var currentGame = listToRemoveFrom[i];
+
+                if (currentGame.Link == link)
+                {
+                    if(this.DebugMode)
+                    {
+                        Console.WriteLine("Removing Game: " + currentGame.Title); ;
+                    }
+
+                    listToRemoveFrom.Remove(currentGame);
+                    break;
+                }
+            }
+        }
+
+        public void AddGame(RssItem rssItem)
+        {
+            var game = new ChessDotComGame();
+            game.Title = rssItem.Title;
+            game.Link = rssItem.Link;
+            game.PubDate = rssItem.PubDate;
+            game.Comments = rssItem.Comments;
+
+            base.Add(game);
+        }
+        public void AddGame(ChessDotComGame game)
+        {
+            base.Add(game);
+        }
+
+        public void AddGame(AtomEntry atomEntry)
+        {
+            foreach (AtomEntry entry in atomEntry.Feed.Entries)
+            {
+                if (atomEntry.Content.Content.Contains("|"))
+                {
+                    var game = new ChessDotComGame();
+
+                    game.Title = entry.Title.Text.ToString();
+                    game.Link = atomEntry.Content.Content.Split('|')[1];
+                    game.Link = game.Link.Remove(game.Link.Length - 1);
+
+                    game.PubDate = atomEntry.Content.Content.Split('|')[2];
+                    game.PubDate = game.PubDate.Remove(game.PubDate.Length - 1);
+
+                    game.Description = atomEntry.Content.Content;
+
+                    if (!this.Contains(game))
+                    {
+                        base.Add(game);
+                    }
+                }
+            }
+        }
+
+        public void AddRange(IEnumerable<AtomEntry> atomEntries)
+        {
+            foreach (var atomEntry in atomEntries)
+            {
+                this.AddGame(atomEntry);
+            }
+        }
+
+        protected new bool Contains(IChessItem chessItem)
+        {
+            bool pubMatch = this.Where(thisGame => thisGame.PubDate == chessItem.PubDate).Any();
+            bool guidMatch = this.Where(thisGame => thisGame.Link == chessItem.Link).Any();
+
+            return pubMatch && guidMatch;
+        }
+    }
+}
---
+++ tags/Revision 1/source/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ChessCalendar
+{
+    class Program
+    {
+        public const string VERSION = "Chess Calendar v10.18.10 ";
+        //public const string CONFIG_FILE_PATH = @"..\..\GamesToLog.xml"; //Not used.. yet
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(VERSION + DateTime.Now.ToShortTimeString());
+
+            ShowDialog:
+            var userInfoForm = new Login_Form(VERSION);
+            userInfoForm.ShowDialog();  
+            
+            //*** Code Execution will stop at this point and wait until user has dismissed the Login form. ***//
+
+            if (userInfoForm.ValidatedForm)
+            {
+                Log.LogVersion = VERSION;
+                Log.DebugMode = userInfoForm.DebugMode;
+                Log.Beep_On_New_Move = userInfoForm.Beep_On_New_Move;
+                Log.Log_All_Games(new Uri("http://www.chess.com/rss/echess/" + userInfoForm.ChessDotComName),
+                                  userInfoForm.User, userInfoForm.Password, userInfoForm.PostURI);
+            }
+            else
+            {
+                MessageBox.Show("Some of the Login information wasn't right, try again.", "ummmmmmm...");
+                goto ShowDialog; //woohoo! lookit that goto!
+            }
+        } 
+    }
+}
