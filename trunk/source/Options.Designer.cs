44e0cfcbd9cc9eee1f568bcb19ee868f913d8dbe
kevingabbert@gmail.com <kevingabbert@gmail.com@0b401191-2a60-48e8-5509-3f5dc672fb8b>
2010-10-19 06:34:46 +0000

    Starting down the *SysTray* path.  Changed to a Win App. Now all functions will be started up by SysTrayApp.
    Console Window will no longer show.
    
    threw down Options form, but it is nonfunctional.

---
+++ trunk/source/Options.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ChessCalendar
+{
+    partial class Options
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Options));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 56);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(122, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Balloon Notifications";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 79);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(85, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Play Sounds";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 33);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(142, 17);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Form-based Notifications";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Options
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(161, 125);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Options";
+            this.Text = "Options";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+
+    }
+}
--- trunk/source/Program.cs
+++ trunk/source/Program.cs
@@ -5,32 +5,12 @@ namespace ChessCalendar
 {
     class Program
     {
-        public const string VERSION = "Chess Calendar v10.18.10 ";
-        //public const string CONFIG_FILE_PATH = @"..\..\GamesToLog.xml"; //Not used.. yet
-
-        static void Main(string[] args)
+        [STAThread]
+        public static void Main(string[] args)
         {
-            Console.WriteLine(VERSION + DateTime.Now.ToShortTimeString());
+            //Console.WriteLine(VERSION + DateTime.Now.ToShortTimeString());
 
-            ShowDialog:
-            var userInfoForm = new Login_Form(VERSION);
-            userInfoForm.ShowDialog();  
-            
-            //*** Code Execution will stop at this point and wait until user has dismissed the Login form. ***//
-
-            if (userInfoForm.ValidatedForm)
-            {
-                Log.LogVersion = VERSION;
-                Log.DebugMode = userInfoForm.DebugMode;
-                Log.Beep_On_New_Move = userInfoForm.Beep_On_New_Move;
-                Log.Log_All_Games(new Uri("http://www.chess.com/rss/echess/" + userInfoForm.ChessDotComName),
-                                  userInfoForm.User, userInfoForm.Password, userInfoForm.PostURI);
-            }
-            else
-            {
-                MessageBox.Show("Some of the Login information wasn't right, try again.", "ummmmmmm...");
-                goto ShowDialog; //woohoo! lookit that goto!
-            }
+            Application.Run(new SysTrayApp());
         } 
     }
 }
