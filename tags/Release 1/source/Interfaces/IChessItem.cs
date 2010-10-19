4521e3bdf9c672f791c89add5b4ae73b03554815
kevingabbert@gmail.com <kevingabbert@gmail.com@0b401191-2a60-48e8-5509-3f5dc672fb8b>
2010-10-19 06:43:12 +0000

    *** Release 1 ***

---
+++ tags/Release 1/source/Interfaces/IChessItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ChessCalendar.Interfaces
+{
+    /// <summary>
+    /// A subset of the RSSItem
+    /// </summary>
+    public interface IChessItem
+    {
+        string Author { set; get; }
+        string Comments { set; get; }
+        string Description { set; get; }
+        string Link { set; get; }
+        string PubDate { set; get; }
+        string Title { set; get; }
+    }
+}
