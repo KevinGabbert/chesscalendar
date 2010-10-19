506d71bfc45450068063f30d88ccd6511dab0009
kevingabbert@gmail.com <kevingabbert@gmail.com@0b401191-2a60-48e8-5509-3f5dc672fb8b>
2010-10-19 06:43:12 +0000

    *** Release 1 ***

---
+++ trunk/tags/Release 1/source/ChessCalendarRSSItems.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using RssToolkit.Rss;
+
+namespace ChessCalendar
+{
+    public class ChessCalendarRSSItems : List<ChessCalendarRSSItem>
+    {
+        public void Add(RssItem rssItem)
+        {
+            var newItem = new ChessCalendarRSSItem();
+            newItem.Author = rssItem.Author;
+            newItem.Comments = rssItem.Comments;
+            newItem.Description = rssItem.Description;
+            newItem.Link = rssItem.Link;
+            newItem.PubDate = rssItem.PubDate;
+            newItem.Title = rssItem.Title;
+
+            base.Add(newItem); 
+        }
+        public void AddRange(IEnumerable<RssItem> rssItems)
+        {
+            foreach (var rssItem in rssItems)
+            {
+                this.Add(rssItem);
+            }
+        }
+    }
+}
