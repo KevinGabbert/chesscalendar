8e9f461b1453741bb4d312a38469bec1f9061d46
kevingabbert@gmail.com <kevingabbert@gmail.com@0b401191-2a60-48e8-5509-3f5dc672fb8b>
2010-10-24 21:24:13 +0000

    reversing how text is shown on Log.  Added scrollbar.

--- source/Forms/ShowLog.Designer.cs
+++ source/Forms/ShowLog.Designer.cs
@@ -31,10 +31,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShowLog));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.txtLog = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.pbTimeTillNextUpdate = new System.Windows.Forms.ProgressBar();
             this.txtNextCheck = new System.Windows.Forms.TextBox();
-            this.txtLog = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
@@ -60,12 +60,22 @@
             this.tabPage1.Text = "Name 1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(0, 0);
+            this.txtLog.MaxLength = 64000;
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(323, 262);
+            this.txtLog.TabIndex = 0;
+            // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(324, 273);
+            this.tabPage2.Size = new System.Drawing.Size(324, 263);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Name 2";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -85,15 +95,6 @@
             this.txtNextCheck.Size = new System.Drawing.Size(330, 20);
             this.txtNextCheck.TabIndex = 3;
             this.txtNextCheck.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // txtLog
-            // 
-            this.txtLog.Location = new System.Drawing.Point(0, 0);
-            this.txtLog.MaxLength = 64000;
-            this.txtLog.Multiline = true;
-            this.txtLog.Name = "txtLog";
-            this.txtLog.Size = new System.Drawing.Size(323, 262);
-            this.txtLog.TabIndex = 0;
             // 
             // ShowLog
             // 
