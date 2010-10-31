80badfc8919edd28548ab95bc7175a0207514175
kevingabbert@gmail.com <kevingabbert@gmail.com@0b401191-2a60-48e8-5509-3f5dc672fb8b>
2010-10-31 20:21:18 +0000

    Created a class to hold the PGNs we download.

---
+++ trunk/source/PGN.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace ChessCalendar
+{
+    public class PGN
+    {
+        string Event { set; get; }
+        string Site { set; get; }
+        string Date { set; get; }
+        string White { set; get; }
+        string Black { set; get; }
+        string Result { set; get; }
+        string WhiteELO { set; get; }
+        string BlackELO { set; get; }
+        string TimeControl { set; get; }
+        string Variant { set; get; }
+        string SetUp { set; get; }
+        string FEN { set; get; }
+
+        string RawMoves { set; get; }
+
+        //Queue<string> Moves
+        //GetCurrentMove
+        //GetLastMove
+        //GetMoveNumber(int)
+    }
+}
