655010aa1f1d1fea0d9dfcc6068a171b0c369b15
kevingabbert@gmail.com <kevingabbert@gmail.com@0b401191-2a60-48e8-5509-3f5dc672fb8b>
2010-11-07 18:44:51 +0000

    tweaked form size and removed extra tab.  This isn't a multi-tab yet, but it will be.

--- trunk/source/Forms/ShowLog.Designer.cs
+++ trunk/source/Forms/ShowLog.Designer.cs
@@ -34,7 +34,6 @@
             this.lnkNoMovesFound = new System.Windows.Forms.LinkLabel();
             this.txtLog = new System.Windows.Forms.TextBox();
             this.dgvAvailableMoves = new System.Windows.Forms.DataGridView();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.pbTimeTillNextUpdate = new System.Windows.Forms.ProgressBar();
             this.txtNextCheck = new System.Windows.Forms.TextBox();
             this.btnPause = new System.Windows.Forms.Button();
@@ -47,11 +46,10 @@
             // tabs
             // 
             this.tabs.Controls.Add(this.tabPage1);
-            this.tabs.Controls.Add(this.tabPage2);
             this.tabs.Location = new System.Drawing.Point(-2, 1);
             this.tabs.Name = "tabs";
             this.tabs.SelectedIndex = 0;
-            this.tabs.Size = new System.Drawing.Size(666, 451);
+            this.tabs.Size = new System.Drawing.Size(776, 451);
             this.tabs.TabIndex = 0;
             // 
             // tabPage1
@@ -62,14 +60,14 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(658, 425);
+            this.tabPage1.Size = new System.Drawing.Size(768, 425);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // lnkNoMovesFound
             // 
             this.lnkNoMovesFound.AutoSize = true;
-            this.lnkNoMovesFound.Location = new System.Drawing.Point(238, 128);
+            this.lnkNoMovesFound.Location = new System.Drawing.Point(329, 182);
             this.lnkNoMovesFound.Name = "lnkNoMovesFound";
             this.lnkNoMovesFound.Size = new System.Drawing.Size(89, 13);
             this.lnkNoMovesFound.TabIndex = 5;
@@ -82,7 +80,7 @@
             this.txtLog.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtLog.Location = new System.Drawing.Point(3, 3);
             this.txtLog.Name = "txtLog";
-            this.txtLog.Size = new System.Drawing.Size(646, 20);
+            this.txtLog.Size = new System.Drawing.Size(758, 20);
             this.txtLog.TabIndex = 4;
             this.txtLog.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -92,25 +90,16 @@
             this.dgvAvailableMoves.Location = new System.Drawing.Point(3, 29);
             this.dgvAvailableMoves.Name = "dgvAvailableMoves";
             this.dgvAvailableMoves.RowHeadersVisible = false;
-            this.dgvAvailableMoves.Size = new System.Drawing.Size(646, 390);
+            this.dgvAvailableMoves.Size = new System.Drawing.Size(758, 390);
             this.dgvAvailableMoves.TabIndex = 0;
             this.dgvAvailableMoves.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAvailableMoves_CellClick);
             this.dgvAvailableMoves.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dgvAvailableMoves_CellFormatting);
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(658, 425);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // pbTimeTillNextUpdate
             // 
             this.pbTimeTillNextUpdate.Location = new System.Drawing.Point(-2, 507);
             this.pbTimeTillNextUpdate.Name = "pbTimeTillNextUpdate";
-            this.pbTimeTillNextUpdate.Size = new System.Drawing.Size(660, 15);
+            this.pbTimeTillNextUpdate.Size = new System.Drawing.Size(776, 15);
             this.pbTimeTillNextUpdate.TabIndex = 1;
             // 
             // txtNextCheck
@@ -118,7 +107,7 @@
             this.txtNextCheck.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtNextCheck.Location = new System.Drawing.Point(-2, 484);
             this.txtNextCheck.Name = "txtNextCheck";
-            this.txtNextCheck.Size = new System.Drawing.Size(660, 20);
+            this.txtNextCheck.Size = new System.Drawing.Size(776, 20);
             this.txtNextCheck.TabIndex = 3;
             this.txtNextCheck.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -134,7 +123,7 @@
             // 
             // btnRefresh
             // 
-            this.btnRefresh.Location = new System.Drawing.Point(386, 458);
+            this.btnRefresh.Location = new System.Drawing.Point(458, 455);
             this.btnRefresh.Name = "btnRefresh";
             this.btnRefresh.Size = new System.Drawing.Size(121, 23);
             this.btnRefresh.TabIndex = 5;
@@ -146,7 +135,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(663, 527);
+            this.ClientSize = new System.Drawing.Size(775, 527);
             this.Controls.Add(this.btnRefresh);
             this.Controls.Add(this.btnPause);
             this.Controls.Add(this.pbTimeTillNextUpdate);
@@ -174,7 +163,6 @@
 
         private System.Windows.Forms.TabControl tabs;
         private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.ProgressBar pbTimeTillNextUpdate;
         private System.Windows.Forms.TextBox txtNextCheck;
         private System.Windows.Forms.DataGridView dgvAvailableMoves;
