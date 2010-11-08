da24b6e72eca2128141099cb88c113d5869e3cb2
kevingabbert@gmail.com <kevingabbert@gmail.com@0b401191-2a60-48e8-5509-3f5dc672fb8b>
2010-11-08 22:04:36 +0000

    more planning

--- trunk/source/CalendarFeed.cs
+++ trunk/source/CalendarFeed.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using ChessCalendar.Interfaces;
+using RssToolkit.Rss;
 
 namespace ChessCalendar
 {
@@ -40,22 +43,13 @@ namespace ChessCalendar
             
         }
 
-
         internal void Go()
         {
-            if (this.Post)
-            {
-                this.Post_NewMoves();
-            }
-
-            if (this.Save)
-            {
-                this.Save_To_Calendar(); //denoted by URI
-            }
+            var postedMoves = this.Post_NewMoves(); //Outputs to NewMove Queue, builds ToDo
+            this.Save_To_Calendar(postedMoves, new Feed(), "", "", new Uri("") ); //denoted by URI   //Processes all items in ToDo
         }
 
         
-
         /// <summary>
         /// Saves new items to the Calendar
         /// </summary>
@@ -73,14 +67,76 @@ namespace ChessCalendar
         /// <summary>
         /// Posts moves in feed to this.Output
         /// </summary>
-        internal void Post_NewMoves()
+        internal List<bool> Post_NewMoves()
         {
             foreach (ChessRSSItem chessRssItem in this)
             {
                 //post
+
+                //this.ToDo = new CalendarManager(this);
+                //this.ToDo.DebugMode = this.DebugMode;
+                //this.ToDo.Beep_On_New_Move = this.Beep_On_New_Move;
+
+                //_calendarToPost = logToCalendar;
+
+                ////load up Ignore list with items already in calendar
+
+                ////get all "auto-logger" entries created in the last 15 days (the max time you can have a game)
+                ////TODO: well, you can actually also go on vacation, which would make it longer but this first version doesn't accomodate for that..
+
+                //this.ToDo.IgnoreList = GoogleCalendar.GetAlreadyLoggedChessGames(userName, password, _calendarToPost, DateTime.Now.Subtract(new TimeSpan(15, 0, 0, 0)), DateTime.Now, "auto-logger");
+
+                //    feedToSave.AddRange(RssDocument.Load(uriToWatch).Channel.Items);
+
+                //    this.AddOrUpdate_Games(this.ToDo, feedToSave);
+
+                //    //TODO On add, and it doesn't already exist, create a reminder. (also create an all day reminder)
+                //    //On remove, delete the all day reminder.
+
+                //    this.ClearList = false;
+
+                //    this.ProcessNewRSSItems(feedToSave); //If there are *new* entries, then the gridview will clear and refresh
+
+                //    //TODO: if NO new entries, it won't clear & refresh, meaning cruft won't 
+
+                //    if (this.LogGames)
+                //    {
+                //        //TODO: this needs to be an asterisk or something on the log form.
+                //        foreach (IChessItem current in this.ToDo)
+                //        {
+                //            this.Output(current); //TODO this causes gridview to clear and refresh
             }
 
             throw new System.NotImplementedException();
+        }
+
+        private void Save_To_Calendar(List<bool> x, Feed feedToSave, string userName, string password, Uri uriToWatch)
+        {
+            //try
+            //{
+            //    if (this.LogGames)
+            //    {
+            //        //TODO: this needs to be an asterisk or something on the log form.
+            //        foreach (IChessItem current in this.ToDo)
+            //        {
+            //            this.Save_Game_Info(current, userName, password);
+            //        }
+            //    }
+
+            //    this.ClearList = true;
+
+            //    this.ToDo.Clear();
+            //}
+            //catch (Exception ex)
+            //{
+            //    //if 504 Invalid gateway error. Chess.com is down
+            //    this.Output(string.Empty, "error. " + ex.Message);
+
+            //    GoogleCalendar.CreateEntry(userName, password, "Error", "Error", "Chess.com error", ex.Message +
+            //                                                                                        this.LogVersion, DateTime.Now, DateTime.Now, _calendarToPost);
+            //}
+
+            //this.Wait(this.WaitSeconds);
         }
     }
 }
