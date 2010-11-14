03c9a3f4d4fcdff51cd6666e7df380e083c2042c
kevingabbert@gmail.com <kevingabbert@gmail.com@0b401191-2a60-48e8-5509-3f5dc672fb8b>
2010-11-14 02:36:53 +0000

    Fleshing out ProcessorTab..

---
+++ trunk/source/Controls/ProcessorTab.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using ChessCalendar.Interfaces;
+
+namespace ChessCalendar.Controls
+{
+    public class ProcessorTab: TabPage
+    {
+        public CalendarProcessor Processor { get; set; }
+        private MessageList MessageList { get; set; }
+
+        private DataGridView _grid;
+        //private ProgressBar _progressBar;
+        //private TextBox _nextCheck;
+
+        private int _fail = 0;
+        //private bool _pause = false;
+
+        private bool _progressBarFlash;
+
+        public ProcessorTab(string tabName, Uri uriToWatch, string userName, string password, Uri logToCalendar)
+        {
+            this.Processor = new CalendarProcessor(uriToWatch, userName, password, logToCalendar, true);
+
+
+
+            _grid = (DataGridView)this.Controls[this.Name + "_dgvAvailableMoves"];
+            //_progressBar = (ProgressBar)this.Controls[this.Name + "_pbTimeTillNextUpdate"];
+            //_nextCheck = (TextBox)this.Controls[this.Name + "_nextCheck"];
+
+            var messages = new TextBox();
+            messages.Name = "TabPage_txtMessages";
+
+            var grid = new DataGridView();
+            grid.Name = "TabPage_Grid";
+            grid.CellClick += this.grid_CellClick;
+
+            var chkLogToCalendar = new CheckBox();
+            chkLogToCalendar.Name = "TabPageName_chkLogToCalendar";
+
+            this.Controls.Add(chkLogToCalendar);
+            this.Controls.Add(grid);
+            this.Controls.Add(messages);
+        }
+
+        public void RefreshRSS()
+        {
+            //this.Update_NextCheck();
+            //this.Update_ProgressBar();
+
+            this.Update_GridView(); //Tells Processor to go read its associated RSS Feed
+
+            Application.DoEvents();
+        }
+
+        private void Update_GridView()
+        {
+            if ((DateTime.Now > this.Processor.NextCheck) || this.Processor.NewMessage)
+            {
+                //this.UpdateText();
+                this.UpdateDataGridView();
+            }
+            else
+            {
+                this.MessageList = new MessageList();
+            }
+        }
+        //private void Update_ProgressBar()
+        //{
+        //    if (this.Processor.NextCheck == new DateTime())
+        //    {
+        //        this._progressBarFlash = !this._progressBarFlash;
+
+        //        this._progressBar.Value = 100;
+        //        this._progressBar.ForeColor = Color.Green;
+
+        //        if (this._progressBarFlash)
+        //        {
+        //            this._progressBar.Show();
+        //            Application.DoEvents();
+        //            Thread.Sleep(100);
+        //        }
+        //        else
+        //        {
+        //            this._progressBar.Hide();
+        //            Application.DoEvents();
+        //            Thread.Sleep(100);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        if (this.Processor.WaitProgress > 100)
+        //        {
+        //            this._progressBar.ForeColor = Color.Red;
+        //            this._progressBar.Value = 100;
+        //        }
+        //        else
+        //        {
+        //            this._progressBar.ForeColor = Color.Blue;
+        //            this._progressBar.Value = this.Processor.WaitProgress;
+        //        }
+
+        //        this._progressBar.Show();
+        //    }
+        //}
+        //private void Update_NextCheck()
+        //{
+        //    //if (this.Processor.NextCheck == new DateTime())
+        //    //{
+        //    //    this._nextCheck.Text = "Unknown";
+        //    //}
+        //    //else
+        //    //{
+        //    //    this._nextCheck.Text = "Next check will be at: " + this.Processor.NextCheck.ToShortTimeString();
+        //    //}
+        //}
+        private void UpdateDataGridView()
+        {
+            if (this.Processor != null)
+            {
+                this.Processor.RefreshRSS();
+
+                if (this.Processor.Output.NewMoves != null)
+                {
+                    if (this.Processor.Output.NewMoves.Updated)
+                    {
+                        if (this.Processor.ClearList)
+                        {
+                            this.MessageList.Clear();
+                        }
+
+                        for (int i = this.Processor.Output.NewMoves.Count; i > 0; i--)
+                        {
+                            this.MessageList.Add(this.Processor.Output.NewMoves.Dequeue());
+                        }
+
+                        GC.Collect();
+                        this.SetMovesDataSource(this.MessageList);
+                        GC.Collect();
+
+                        this.Processor.NewMessage = true;
+                    }
+                    else
+                    {
+                        if (this.Processor.Count > 0)
+                        {
+                            if (this.Processor.Count < 1)
+                            {
+                                this.MessageList.Clear();
+                                this.SetMovesDataSource(this.MessageList);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    _fail += 1;
+                    //this.txtLog.Text = "Log is Null: " + _fail + Environment.NewLine + this.txtLog.Text;
+
+                    //if (this.txtLog.Text.Length > 5001)
+                    //{
+                    //    this.txtLog.Text.Remove(5000);
+                    //}
+                }
+            }
+        }
+        //private void UpdateText()
+        //{
+        //    if (this.Processor != null)
+        //    {
+        //        if (this.Processor.Messages != null)
+        //        {
+        //            if (this.Processor.Messages.Count > 0)
+        //            {
+        //                this.txtLog.Text = (this.Processor.Messages.Dequeue() + Environment.NewLine + this.txtLog.Text);
+
+        //                if (this.txtLog.Text.Length > 5001)
+        //                {
+        //                    this.txtLog.Text.Remove(5000);
+        //                }
+
+        //                this.Processor.NewMessage = true;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            this.txtLog.Text += "Log is Null" + Environment.NewLine;
+        //        }
+        //    }
+        //}
+
+        private static void FormatDataGrid(DataGridView dataGrid)
+        {
+            dataGrid.Columns.Clear();
+            dataGrid.AutoGenerateColumns = false;
+
+            DataGridViewLinkColumn newMoveColumn = new DataGridViewLinkColumn();
+            newMoveColumn.DataPropertyName = "NewMove";
+            newMoveColumn.HeaderText = "X";
+            newMoveColumn.Width = 20;
+            newMoveColumn.Visible = false;
+
+            DataGridViewTextBoxColumn pubDateColumn = new DataGridViewTextBoxColumn();
+            pubDateColumn.DataPropertyName = "GetPubDate";
+            pubDateColumn.HeaderText = "Pub Date";
+            pubDateColumn.Width = 125;
+
+            DataGridViewLinkColumn titleColumn = new DataGridViewLinkColumn();
+            titleColumn.DataPropertyName = "GameTitle"; //GameTitle
+            titleColumn.HeaderText = "Title";
+            titleColumn.Width = 220;
+            titleColumn.LinkBehavior = LinkBehavior.SystemDefault;
+
+            DataGridViewTextBoxColumn opponentColumn = new DataGridViewTextBoxColumn();
+            opponentColumn.DataPropertyName = "Opponent";
+            opponentColumn.HeaderText = "Opponent";
+            opponentColumn.Width = 100;
+
+            DataGridViewTextBoxColumn ratingColumn = new DataGridViewTextBoxColumn();
+            ratingColumn.DataPropertyName = "RatingRaw";
+            ratingColumn.HeaderText = "Rating";
+            ratingColumn.Width = 80;
+
+            DataGridViewTextBoxColumn timeLeftColumn = new DataGridViewTextBoxColumn();
+            timeLeftColumn.DataPropertyName = "TimeLeftRaw";
+            timeLeftColumn.HeaderText = "Time Left";
+            timeLeftColumn.Width = 150;
+
+            DataGridViewTextBoxColumn moveColumn = new DataGridViewTextBoxColumn();
+            moveColumn.DataPropertyName = "MoveRaw";
+            moveColumn.HeaderText = "Move #";
+            moveColumn.Width = 80;
+
+            //Do I *really* have to make a hidden column?
+            DataGridViewLinkColumn gameIDColumn = new DataGridViewLinkColumn();
+            gameIDColumn.DataPropertyName = "GameLink"; //GameID
+            gameIDColumn.HeaderText = "Game";
+            gameIDColumn.Width = 350;
+            gameIDColumn.Visible = false;
+
+            dataGrid.Columns.Add(newMoveColumn);
+            dataGrid.Columns.Add(pubDateColumn);
+            dataGrid.Columns.Add(titleColumn);
+            dataGrid.Columns.Add(opponentColumn);
+            dataGrid.Columns.Add(ratingColumn);
+            dataGrid.Columns.Add(timeLeftColumn);
+            dataGrid.Columns.Add(moveColumn);
+
+            dataGrid.Columns.Add(gameIDColumn);
+        }
+
+        private delegate void MovesDelegate(BindingList<IChessItem> dataSource);
+        private void SetMovesDataSource(BindingList<IChessItem> dataSource)
+        {
+            if (_grid.InvokeRequired)
+            {
+                _grid.Invoke(new MovesDelegate(this.SetMovesDataSource), dataSource);
+            }
+            else
+            {
+                _grid.DataSource = dataSource;
+            }
+        }
+
+        private void grid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            switch (e.ColumnIndex)
+            {
+                case 2:
+                    //Do we really have to use a hidden column? is this the only way?
+                    Process.Start(_grid[7, e.RowIndex].Value.ToString());
+                    break;
+            }
+        }
+
+        //private Control FindControlByName(string name)
+        //{
+        //    return this.Controls.Cast<Control>().FirstOrDefault(c => c.Name == name);
+        //}
+    }
+}
--- trunk/source/Forms/ShowLog.Designer.cs
+++ trunk/source/Forms/ShowLog.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShowLog));
             this.tabs = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.chkRecordGame = new System.Windows.Forms.CheckBox();
             this.lnkNoMovesFound = new System.Windows.Forms.LinkLabel();
             this.txtLog = new System.Windows.Forms.TextBox();
             this.dgvAvailableMoves = new System.Windows.Forms.DataGridView();
@@ -39,7 +40,8 @@
             this.btnPause = new System.Windows.Forms.Button();
             this.btnRefresh = new System.Windows.Forms.Button();
             this.btnAddFeed = new System.Windows.Forms.Button();
-            this.chkRecordGame = new System.Windows.Forms.CheckBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabs.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAvailableMoves)).BeginInit();
@@ -48,24 +50,39 @@
             // tabs
             // 
             this.tabs.Controls.Add(this.tabPage1);
+            this.tabs.Controls.Add(this.tabPage2);
             this.tabs.Location = new System.Drawing.Point(-2, 1);
             this.tabs.Name = "tabs";
             this.tabs.SelectedIndex = 0;
-            this.tabs.Size = new System.Drawing.Size(797, 451);
+            this.tabs.Size = new System.Drawing.Size(797, 499);
             this.tabs.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.chkRecordGame);
             this.tabPage1.Controls.Add(this.lnkNoMovesFound);
+            this.tabPage1.Controls.Add(this.btnPause);
             this.tabPage1.Controls.Add(this.txtLog);
+            this.tabPage1.Controls.Add(this.txtNextCheck);
             this.tabPage1.Controls.Add(this.dgvAvailableMoves);
+            this.tabPage1.Controls.Add(this.pbTimeTillNextUpdate);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(789, 425);
+            this.tabPage1.Size = new System.Drawing.Size(789, 473);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // chkRecordGame
+            // 
+            this.chkRecordGame.AutoSize = true;
+            this.chkRecordGame.Location = new System.Drawing.Point(3, 402);
+            this.chkRecordGame.Name = "chkRecordGame";
+            this.chkRecordGame.Size = new System.Drawing.Size(202, 17);
+            this.chkRecordGame.TabIndex = 22;
+            this.chkRecordGame.Text = "Record this game in Google Calendar";
+            this.chkRecordGame.UseVisualStyleBackColor = true;
             // 
             // lnkNoMovesFound
             // 
@@ -93,30 +110,30 @@
             this.dgvAvailableMoves.Location = new System.Drawing.Point(3, 29);
             this.dgvAvailableMoves.Name = "dgvAvailableMoves";
             this.dgvAvailableMoves.RowHeadersVisible = false;
-            this.dgvAvailableMoves.Size = new System.Drawing.Size(783, 369);
+            this.dgvAvailableMoves.Size = new System.Drawing.Size(783, 363);
             this.dgvAvailableMoves.TabIndex = 0;
             this.dgvAvailableMoves.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAvailableMoves_CellClick);
             this.dgvAvailableMoves.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dgvAvailableMoves_CellFormatting);
             // 
             // pbTimeTillNextUpdate
             // 
-            this.pbTimeTillNextUpdate.Location = new System.Drawing.Point(-2, 507);
+            this.pbTimeTillNextUpdate.Location = new System.Drawing.Point(3, 451);
             this.pbTimeTillNextUpdate.Name = "pbTimeTillNextUpdate";
-            this.pbTimeTillNextUpdate.Size = new System.Drawing.Size(797, 15);
+            this.pbTimeTillNextUpdate.Size = new System.Drawing.Size(780, 15);
             this.pbTimeTillNextUpdate.TabIndex = 1;
             // 
             // txtNextCheck
             // 
             this.txtNextCheck.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtNextCheck.Location = new System.Drawing.Point(-2, 484);
+            this.txtNextCheck.Location = new System.Drawing.Point(3, 425);
             this.txtNextCheck.Name = "txtNextCheck";
-            this.txtNextCheck.Size = new System.Drawing.Size(797, 20);
+            this.txtNextCheck.Size = new System.Drawing.Size(778, 20);
             this.txtNextCheck.TabIndex = 3;
             this.txtNextCheck.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // btnPause
             // 
-            this.btnPause.Location = new System.Drawing.Point(334, 458);
+            this.btnPause.Location = new System.Drawing.Point(321, 402);
             this.btnPause.Name = "btnPause";
             this.btnPause.Size = new System.Drawing.Size(121, 23);
             this.btnPause.TabIndex = 4;
@@ -126,7 +143,7 @@
             // 
             // btnRefresh
             // 
-            this.btnRefresh.Location = new System.Drawing.Point(670, 458);
+            this.btnRefresh.Location = new System.Drawing.Point(662, 506);
             this.btnRefresh.Name = "btnRefresh";
             this.btnRefresh.Size = new System.Drawing.Size(121, 23);
             this.btnRefresh.TabIndex = 5;
@@ -136,7 +153,7 @@
             // 
             // btnAddFeed
             // 
-            this.btnAddFeed.Location = new System.Drawing.Point(2, 458);
+            this.btnAddFeed.Location = new System.Drawing.Point(2, 506);
             this.btnAddFeed.Name = "btnAddFeed";
             this.btnAddFeed.Size = new System.Drawing.Size(121, 23);
             this.btnAddFeed.TabIndex = 6;
@@ -144,26 +161,31 @@
             this.btnAddFeed.UseVisualStyleBackColor = true;
             this.btnAddFeed.Click += new System.EventHandler(this.btnAddFeed_Click);
             // 
-            // chkRecordGame
+            // tabPage2
             // 
-            this.chkRecordGame.AutoSize = true;
-            this.chkRecordGame.Location = new System.Drawing.Point(3, 402);
-            this.chkRecordGame.Name = "chkRecordGame";
-            this.chkRecordGame.Size = new System.Drawing.Size(202, 17);
-            this.chkRecordGame.TabIndex = 22;
-            this.chkRecordGame.Text = "Record this game in Google Calendar";
-            this.chkRecordGame.UseVisualStyleBackColor = true;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Size = new System.Drawing.Size(789, 473);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(660, 398);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // ShowLog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(795, 527);
+            this.ClientSize = new System.Drawing.Size(795, 533);
             this.Controls.Add(this.btnAddFeed);
             this.Controls.Add(this.btnRefresh);
-            this.Controls.Add(this.btnPause);
-            this.Controls.Add(this.pbTimeTillNextUpdate);
-            this.Controls.Add(this.txtNextCheck);
             this.Controls.Add(this.tabs);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ShowLog";
@@ -179,7 +201,6 @@
             this.tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvAvailableMoves)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -196,5 +217,7 @@
         private System.Windows.Forms.LinkLabel lnkNoMovesFound;
         private System.Windows.Forms.Button btnAddFeed;
         private System.Windows.Forms.CheckBox chkRecordGame;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
